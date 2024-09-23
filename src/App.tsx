e1bdca8568a54c66fe7bebe010d37567daaa50a6
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2024-09-23 10:47:56 -0300

    add noew image and plots

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,67 @@
+import Chart from './components/chart'
+import { ScrollArea } from './components/ui/scroll-area'
+
+const charts = [
+  {
+    date: '2024-8-19',
+    title: 'Arc Diagram',
+    description:
+      'Graph showing the relationship between the number of male and female students in each plan.',
+    from: {
+      label: 'RawGraphs',
+      link: 'https://app.rawgraphs.io/',
+      color: 'text-green-400',
+    },
+    img: ['./charts/arc_diagram.svg'],
+  },
+  {
+    date: '2024-9-23',
+    title: 'Proyecto informacion personal',
+    description:
+      'This barchart shows the relationship between heartbeats and the course I had that day',
+    from: {
+      label: 'Observable',
+      link: 'https://observablehq.com/d/654c713d121c433b',
+      color: 'text-red-400',
+    },
+    img: ['./charts/stackedbar_courses_heart.svg', './charts/sleep_times.svg'],
+  },
+]
+
 function App() {
   return (
-    <div className="dark flex h-screen w-full flex-col items-center gap-4 bg-background text-foreground">
-      <div className="flex items-center justify-center gap-4 p-4">
-        <span className="font-bold text-red-400">
-          ab<span className="font-bold text-neutral-200">enve</span>
-        </span>
-
-        <img
-          className="inline-block h-8 w-8 border-spacing-8 rounded-full border border-border"
-          src="./images/ab-square.png"
-          alt="abenve"
-        />
-      </div>
-      <div className="flex max-w-[30rem] flex-col items-center gap-2">
-        <img
-          src="./charts/arc_diagram.svg"
-          alt="arc diagram"
-          className="w-[30rem] rounded-md"
-        />
-        <div className="flex w-full flex-col gap-1">
-          <span className="text-base text-foreground">
-            Graph showing the relationship between the number of male and female
-            students in each plan.
+    <div className="dark flex min-h-screen w-full flex-col gap-8 bg-background pb-10 text-foreground">
+      <nav className="w-full border border-solid border-b-border">
+        <div className="flex items-center justify-center gap-4 p-4">
+          <span className="font-bold text-red-400">
+            ab<span className="font-bold text-neutral-200">enve</span>
           </span>
-          <small className="text-sm text-muted-foreground">
-            19 / 8 - Create from{' '}
-            <a
-              target="_blank"
-              className="text-green-400 hover:cursor-pointer hover:underline"
-              href="https://app.rawgraphs.io/"
-            >
-              RawGraphs
-            </a>
-          </small>
+
+          <img
+            className="inline-block h-8 w-8 border-spacing-8 rounded-full border border-border"
+            src="./images/ab-square.png"
+            alt="abenve"
+          />
         </div>
-      </div>
+      </nav>
+      <ScrollArea className="min-h-full">
+        <div className="flex flex-col items-center gap-8">
+          {charts
+            .sort(
+              (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+            )
+            .map((chart, index) => (
+              <Chart
+                key={index}
+                date={new Date(chart.date)}
+                title={chart.title}
+                description={chart.description}
+                from={chart.from}
+                img={chart.img}
+              />
+            ))}
+        </div>
+      </ScrollArea>
     </div>
   )
 }
--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,22 +1,54 @@
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import { cn } from '@/lib/utils'
+import { ScrollArea } from './ui/scroll-area'
 
-function ChartsMasonry() {
+interface ChartProps {
+  date: Date
+  title: string
+  description: string
+  from: {
+    label: string
+    link: string
+    color: string
+  }
+  img: string[]
+}
+
+function Chart({ date, title, description, from, img }: ChartProps) {
   return (
-    <ResponsiveMasonry
-      columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
-      className="bg-red-400"
-    >
-      <Masonry className="bg-blue-300">
-        <div className="h-60 w-20 rounded-lg bg-amber-400" />
-        <div className="h-42 w-52 rounded-lg bg-amber-500" />
-        <div className="h-64 w-64 rounded-lg bg-amber-600" />
-        <div className="w-22rounded-lg h-20 bg-amber-200" />
-        <div className="h-52 w-32 rounded-lg bg-amber-100" />
-        <div className="h-32 w-16 rounded-lg bg-amber-300" />
-        <div className="h-36 w-2 rounded-lg bg-amber-600" />
-      </Masonry>
-    </ResponsiveMasonry>
+    <div className="flex flex-col items-center gap-2">
+      {img.length > 0 ? (
+        <ScrollArea className="w-full">
+          <div className="flex w-full items-center gap-2">
+            {img.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt="arc diagram"
+                className="max-w-[30rem] rounded-md bg-white"
+              />
+            ))}
+          </div>
+        </ScrollArea>
+      ) : null}
+
+      <div className="flex w-full max-w-[30rem] flex-col gap-1">
+        <h2 className="text-lg font-semibold text-foreground">{title}</h2>
+        <p className="text-base text-foreground">{description}</p>
+        <small className="text-xs text-muted-foreground">
+          {new Date(date).toLocaleDateString()} - Create from{' '}
+          {
+            <a
+              target="_blank"
+              className={cn('hover:cursor-pointer hover:underline', from.color)}
+              href={from.link}
+            >
+              {from.label}
+            </a>
+          }
+        </small>
+      </div>
+    </div>
   )
 }
 
-export default ChartsMasonry
+export default Chart
